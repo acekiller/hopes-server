90a47ced62debdd4701e485435c39aa04716225c
acekiller <fengxijun51020@hotmail.com>
2023-08-25 08:37:39 +0800

    add config.py

--- src/create_app.py
+++ src/create_app.py
@@ -2,15 +2,16 @@ from flask import Flask
 from database import mdb
 from auth import auth
 from redis import Redis
+import config as config
 
 app = Flask(__name__)
-redis_store = Redis(host='localhost', port=6379, db=0)
+redis_store = Redis(host=config.redis_host, port=config.redis_port, db=0)
 
 class App():
     @classmethod
     def create_app(cls):
-        app.secret_key = 'cgadafe0129018'
-        app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{mdb.user}:{mdb.password}@{mdb.host}:{mdb.port}/{mdb.database}?charset=utf8mb4"
+        app.secret_key = config.app_key
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{config.mysql_user}:{config.mysql_password}@{config.mysql_host}:{config.mysql_port}/{config.mysql_database}?charset=utf8mb4"
         app.config['SQLALCHEMY_ECHO'] = True
         app.config['JSON_AS_ASCII'] = False
         app.config['JWT_SECRET_KEY'] = app.secret_key
--- src/database/mdb.py
+++ src/database/mdb.py
@@ -1,10 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 
-host = '127.0.0.1'
-port = 3306
-user = 'root'
-password = 'fxj281314'
-database = 'hopes'
-
 db = SQLAlchemy()
 
