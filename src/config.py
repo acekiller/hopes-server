8b08b4cc9647dcd724a78431c3f290db89c9d510
acekiller <fengxijun51020@hotmail.com>
2023-08-25 08:38:14 +0800

    add config.py

---
+++ src/config.py
@@ -0,0 +1,16 @@
+app_key = "app_key"
+
+'''
+mysql config
+'''
+mysql_host = 'host_name'
+mysql_port = 3306
+mysql_user = 'username'
+mysql_password = 'password'
+mysql_database = 'database_name'
+
+'''
+redis config
+'''
+redis_host = 'host_name'
+redis_port = 6379
